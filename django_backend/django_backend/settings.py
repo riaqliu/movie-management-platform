c2534cfdd93c8c77ae67667d40f7514fa696de3c
jav <javquilario074@gmail.com>
2025-07-01 21:55:28 +0800

    Add pagination

--- django_backend/django_backend/settings.py
+++ django_backend/django_backend/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
