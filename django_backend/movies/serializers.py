e73f9781033870b1fcd01d32c99459e0b83daf11
jav <javquilario074@gmail.com>
2025-07-01 21:47:57 +0800

    Validate file type for serializer

--- django_backend/movies/serializers.py
+++ django_backend/movies/serializers.py
@@ -6,3 +6,8 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_video_file(self, file):
+        if not file.name.lower().endswith('.mp4'):
+            raise serializers.ValidationError("Unsupported file type")
+        return file
